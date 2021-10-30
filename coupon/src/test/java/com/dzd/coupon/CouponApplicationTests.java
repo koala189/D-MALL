3d20b5520d4fab58a54be6fd7203280caf135896
koala <1481936030@qq.com>
2021-10-30 20:04:43 +0800

    first commit:project structure construction

---
+++ coupon/src/test/java/com/dzd/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzd.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
